ee3ae5381dc04a31600d805f55538c9dee7e9185
世古侑大 <yseko0825@gmail.com>
2025-12-28 22:33:05 +0900

    検索バー配置

--- app/components/layout/AppHeader.tsx
+++ app/components/layout/AppHeader.tsx
@@ -1,8 +1,10 @@
+import HeaderSearch from "../search/HeaderSearch";
+
 const AppHeader = () => {
     return (
         <header className="flex bg-black h-20 items-center">
             <h1 className="flex-1 text-white">TCG Manager</h1>
-            <div className="flex-1">01</div>
+            <HeaderSearch />
             <div className="flex-1">02</div>
         </header>
     )
